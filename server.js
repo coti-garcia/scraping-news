00484d248d97746eb3dc836eeca8361630427a39
coti_garcia <javiera.garcialatorre@gmail.com>
2019-09-11 22:43:54 -0400

    Scraping working

--- server.js
+++ server.js
@@ -1,15 +1,35 @@
 const express = require("express")
 const path = require("path")
 const axios = require("axios")
-const PORT = process.env.PORT || 3030;
+const mongoose = require('mongoose');
+const cheerio = require('cheerio')
+
+mongoose.connect('mongodb://localhost/scrapingrNews', {useNewUrlParser: true});
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true});
+
+
+app.get("/scraper", function(req,res){
+    axios.get("https://www.washingtonpost.com").then( response => {
+        const $ = cheerio.load(response.data)
+        const news = [];
+        $(".top-table .flex-item").each(function(i,element){
+            let item = {
+                title : $(element).find(".headline a").text(),
+                body : $(element).find(".blurb").text()
+            } 
+            
+            news.push(item);
+        })
+        console.log(news.length)
+        res.json(news);
+    })
+})
 
 app.get("/saved", function(req,res){
     res.sendFile(path.join(__dirname, "./public/saved.html"))
