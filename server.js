8608e3d264e45379904241248f66ad32b1823bd9
coti_garcia <javiera.garcialatorre@gmail.com>
2019-09-12 21:08:51 -0400

    express handle-bars added

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
-const express = require("express")
+const express = require("express");
+const exphbs  = require('express-handlebars');
 const path = require("path")
 const axios = require("axios")
 const mongoose = require('mongoose');
-const cheerio = require('cheerio')
+const cheerio = require('cheerio');
 
 mongoose.connect('mongodb://localhost/scrapingrNews', { useUnifiedTopology: true });
 const PORT = process.env.PORT || 5000;
@@ -12,7 +13,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-
+app.engine('handlebars', exphbs( {defaultLayout: "main"} ));
+app.set('view engine', 'handlebars');
+ 
+app.get('/', function (req, res) {
+    res.render('home');
+});
 
 app.get("/scraper", function(req,res){
     axios.get("https://www.npr.org/sections/news/")
@@ -25,7 +31,6 @@ app.get("/scraper", function(req,res){
                 section: $(element).find(".slug a").text(),
                 body : $(element).find(".teaser a").text()
             } 
-            
             news.push(item);
         })
         console.log(news.length)
@@ -37,8 +42,9 @@ app.get("/scraper", function(req,res){
 })
 
 app.get("/saved", function(req,res){
-    res.sendFile(path.join(__dirname, "./public/saved.html"))
+    res.render('saved');
 })
+
 
 app.listen( PORT, function(){
     console.log(`Listening on http://localhost:${PORT}`)
