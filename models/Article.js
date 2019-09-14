c071316de1ebda034076b6b37d483edfdf99fe22
coti_garcia <javiera.garcialatorre@gmail.com>
2019-09-13 20:07:42 -0400

    Update DB with saved articles and render them into the DOM

--- models/Article.js
+++ models/Article.js
@@ -1,13 +1,14 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const ArticleSchema = new Schema({
-        title : String,
-        body: String,
-        section: String,
-        // note: {
-        //     type: Schema.Type.ObjectID,
-        //     ref: "Note"
-        // }
-})
+  title: String,
+  body: String,
+  section: String,
+  isSaved: Boolean,
+  note: {
+    type: Schema.Types.ObjectId,
+    ref: "Note"
+  }
+});
 const Article = mongoose.model("Article", ArticleSchema);
 module.exports = Article;
--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require("express");
-const exphbs  = require('express-handlebars');
-const path = require("path")
-const axios = require("axios")
-const mongoose = require('mongoose');
-const cheerio = require('cheerio');
-const db = require("./models")
-mongoose.connect('mongodb://localhost/scrapingrNews', { useUnifiedTopology: true });
+const exphbs = require("express-handlebars");
+const path = require("path");
+const axios = require("axios");
+const mongoose = require("mongoose");
+const cheerio = require("cheerio");
+const db = require("./models");
+mongoose.connect("mongodb://localhost/scrapingrNews", {
+  useUnifiedTopology: true
+});
 const PORT = process.env.PORT || 5000;
 
 const app = express();
@@ -13,57 +15,95 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-app.engine('handlebars', exphbs( {defaultLayout: "main"} ));
-app.set('view engine', 'handlebars');
- 
-app.get('/', function (req, res) {
-    res.render('home', {title: 'Scraping News'});
-});
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
-app.get("/saved", function(req,res){
-    res.render('saved', {title: 'Saved News'});
-})
-
-app.get("/scraper", function(req,res){
-    axios.get("https://www.npr.org/sections/news/")
-    .then( response => {
-        const $ = cheerio.load(response.data)
-        let news =[]
-        $(".item").each(function(i,element){
-            let item = {
-                title : $(element).find(".title a").text(),
-                section: $(element).find(".slug a").text(),
-                body : $(element).find(".teaser a").text()
-            } 
-            db.Article.create(item).then(function(doc){
-                console.log(item);
-            })
-        }).then(function(){
-            res.send("Scraper Complete");
-        })
+app.get("/", function(req, res) {
+  db.Article.find({})
+    .then(function(data) {
+      res.render("articles", { articles: data, title: "Scraping News" });
     })
     .catch(function(err) {
-        res.json({ msg: err });;
+      // COME BACK TO THIS
+      res.json({ msg: err });
     });
-})
+});
 
-app.get("/articles", function(req,res){
-    db.Article.find({}).then(function(data){
-        res.send(data)
-    }).catch(function(err) {
-        res.json({ msg: err });;
+app.get("/saved", function(req, res) {
+  db.Article.find({ isSaved: true })
+    .then(function(data) {
+      res.render("articles", { articles: data, title: "Saved News" });
+    })
+    .catch(function(err) {
+      // COME BACK TO THIS
+      res.json({ msg: err });
     });
-})
+});
 
-app.get("/deleteAll", function(req,res){
-    db.Article.deleteMany({}).then(function(doc){
-        res.json(doc)
-    }).catch(function(err) {
-        res.json({ msg: err });;
+app.get("/scraper", function(req, res) {
+  axios
+    .get("https://www.npr.org/sections/news/")
+    .then(response => {
+      const $ = cheerio.load(response.data);
+      let news = [];
+      $(".item")
+        .each(function(i, element) {
+          let item = {
+            title: $(element)
+              .find(".title a")
+              .text(),
+            section: $(element)
+              .find(".slug a")
+              .text(),
+            body: $(element)
+              .find(".teaser a")
+              .text(),
+            isSaved: false
+          };
+          db.Article.create(item).then(function(doc) {
+            console.log(item);
+          });
+        })
+        .then(function() {
+          res.send("Scraper Complete");
+        });
+    })
+    .catch(function(err) {
+      res.json({ msg: err });
     });
-})
+});
 
+app.get("/articles", function(req, res) {
+  db.Article.find({})
+    .then(function(data) {
+      res.send(data);
+    })
+    .catch(function(err) {
+      res.json({ msg: err });
+    });
+});
 
-app.listen( PORT, function(){
-    console.log(`Listening on http://localhost:${PORT}`)
-})
+app.delete("/articles", function(req, res) {
+  db.Article.deleteMany({})
+    .then(function(doc) {
+      res.json(doc);
+    })
+    .catch(function(err) {
+      res.json({ msg: err });
+    });
+});
+
+app.put("/articles/:id", function(req, res) {
+  const id = req.params.id;
+  db.Article.findOneAndUpdate({ _id: id }, { isSaved: true }, { new: true })
+    .then(function(data) {
+      res.send(data);
+    })
+    .catch(function(err) {
+      res.json({ msg: err });
+    });
+});
+
+app.listen(PORT, function() {
+  console.log(`Listening on http://localhost:${PORT}`);
+});
